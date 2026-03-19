9b33546f14611eba656da5b289a6b81e167ad0ce
Your Name <you@example.com>
2026-03-19 14:32:47 +0800

    增加开关切换两种特征计算引擎

--- cpp/metaspore/sparse_feature_extractor.cpp
+++ cpp/metaspore/sparse_feature_extractor.cpp
@@ -16,29 +16,64 @@
 
 #include <sstream>
 #include <stdexcept>
-#include <common/utils.h>
 #include <common/arrow/arrow_helpers.h>
+#include <common/features/schema_parser.h>
+#include <common/threadpool.h>
+#include <common/utils.h>
 #include <metaspore/stack_trace_utils.h>
 #include <metaspore/sparse_feature_extractor.h>
+#include <boost/asio/use_future.hpp>
 #include <spdlog/spdlog.h>
 
 namespace metaspore {
+
+DECLARE_bool(use_lightweight_feature_compute);
 
 SparseFeatureExtractor::SparseFeatureExtractor(const std::string &source_table_name,
                                                const std::string &schema_source)
     : source_table_name_(source_table_name)
     , schema_source_(schema_source)
-    , feature_count_(0) {
+    , feature_count_(0)
+    , use_lightweight_(FLAGS_use_lightweight_feature_compute) {
     std::istringstream stream(schema_source);
-    auto the_status = executor_.parse_schema(stream, feature_count_);
+    status the_status;
+    if (use_lightweight_) {
+        the_status = lightweight_executor_.parse_schema(stream, feature_count_);
+    } else {
+        the_status = arrow_executor_.add_source(source_table_name_);
+        if (the_status.ok()) {
+            the_status = FeatureSchemaParser::parse_hash_and_combine(
+                stream, arrow_executor_, feature_count_);
+        }
+    }
     check_construct(the_status);
 }
 
 std::tuple<std::vector<uint64_t>, std::vector<uint64_t>>
 SparseFeatureExtractor::extract(std::shared_ptr<arrow::RecordBatch> batch) {
-    auto result = executor_.execute(batch);
-    check_extract(result.status());
-    std::shared_ptr<arrow::RecordBatch> result_batch = *result;
+    std::shared_ptr<arrow::RecordBatch> result_batch;
+    if (use_lightweight_) {
+        auto result = lightweight_executor_.execute(batch);
+        check_extract(result.status());
+        result_batch = *result;
+    } else {
+        auto fn = [&]() -> awaitable_result<std::shared_ptr<arrow::RecordBatch>> {
+            ASSIGN_RESULT_OR_CO_RETURN_NOT_OK(auto ctx, arrow_executor_.start_plan());
+            CALL_AND_CO_RETURN_IF_STATUS_NOT_OK(
+                arrow_executor_.set_input_schema(ctx, source_table_name_, batch->schema()));
+            CALL_AND_CO_RETURN_IF_STATUS_NOT_OK(arrow_executor_.build_plan(ctx));
+            CALL_AND_CO_RETURN_IF_STATUS_NOT_OK(
+                arrow_executor_.feed_input(ctx, source_table_name_, batch));
+            Defer _([&] { (void)arrow_executor_.finish_plan(ctx); });
+            CO_ASSIGN_RESULT_OR_CO_RETURN_NOT_OK(auto result, arrow_executor_.execute(ctx));
+            co_return result;
+        };
+        auto &tp = Threadpools::get_background_threadpool();
+        auto fut = boost::asio::co_spawn(tp, std::move(fn), boost::asio::use_future);
+        auto result = fut.get();
+        check_extract(result.status());
+        result_batch = *result;
+    }
     std::vector<uint64_t> indices;
     std::vector<uint64_t> offsets;
     auto r = flatten_arrow_batch<std::vector>(result_batch, indices, offsets);
--- cpp/metaspore/sparse_feature_extractor.h
+++ cpp/metaspore/sparse_feature_extractor.h
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+#include <common/features/feature_compute_exec.h>
 #include <common/features/lightweight_feature_compute.h>
 
 namespace metaspore {
@@ -38,7 +39,9 @@ private:
     std::string source_table_name_;
     std::string schema_source_;
     int feature_count_;
-    LightweightFeatureCompute executor_;
+    bool use_lightweight_;
+    LightweightFeatureCompute lightweight_executor_;
+    FeatureComputeExec arrow_executor_;
 };
 
 } // namespace metaspore
